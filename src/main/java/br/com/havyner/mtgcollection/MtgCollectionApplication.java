207362b1d8a75dbeb7214a82570fc7fef1a13d53
Havyner Caetano <havyner@outlook.com>
2020-04-10 18:50:22 -0300

    Add starter files for project

---
+++ src/main/java/br/com/havyner/mtgcollection/MtgCollectionApplication.java
@@ -0,0 +1,13 @@
+package br.com.havyner.mtgcollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtgCollectionApplication.class, args);
+	}
+
+}
