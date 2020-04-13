e5c8aab6cd6aacd1a6411c54435d8c6b10f845cb
Havyner Caetano <hrcgeo@gmail.com>
2020-04-13 17:56:12 -0300

    Change folder structure to back and front-end

---
+++ front-end/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Home
+      </header>
+    </div>
+  );
+}
+
+export default App;
