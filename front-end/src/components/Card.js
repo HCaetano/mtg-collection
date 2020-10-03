17ff557181d6e95b213f54572b7a8a6dfe34480e
HCaetano <hrcgeo@gmail.com>
2020-10-02 21:40:42 -0300

    Add Card component that removes card rendering from Home

---
+++ front-end/src/components/Card.js
@@ -0,0 +1,24 @@
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+
+class Card extends Component {
+  render() {
+    const { card } = this.props;
+
+    return (
+      <article>
+        <p>{ card.name }</p>
+        <p>{ card.manaCost }</p>
+        <p>{ card.cmc }</p>
+        <p>{ card.typepne }</p>
+        <p>{ card.oracleText }</p>
+        <p>{ card.colors }</p>
+        <p>{ card.magicSetName }</p>
+        <p>{ card.rarity }</p>
+        <button onClick={this.deleteCard} value={card.id}>Delete</button>
+      </article>
+    );
+  }
+}
+
+export default Card;
