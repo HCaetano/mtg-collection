6563a38b570398008cf716a00ddd7f3468452596
HCaetano <hrcgeo@gmail.com>
2020-12-21 17:41:36 -0300

    Add conditional rendering to Card and CardForm, on Home only shows card's image

--- front-end/src/components/Card.js
+++ front-end/src/components/Card.js
@@ -1,22 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import '../css/Card.css';
 class Card extends Component {
   render() {
     const { card, deleteCard } = this.props;
+    const showDetails = window.location.pathname !== '/' ? true : false;
 
     return (
       <article className="card-element">
-        <p>{ card.name }</p>
-        <p>{ card.manaCost }</p>
-        <p>{ card.cmc }</p>
-        <p>{ card.typepne }</p>
-        <p>{ card.oracleText }</p>
-        <p>{ card.colors }</p>
-        <p>{ card.magicSetName }</p>
-        <p>{ card.rarity }</p>
-        <img src={ card.image } />
-        <button onClick={deleteCard} value={card.id}>Delete</button>
+        {
+          showDetails
+            ? (
+                <Fragment>
+                  <p>{card.name}</p>
+                  <p>{card.manaCost}</p>
+                  <p>{card.typeLine}</p>
+                  <p>{card.oracleText}</p>
+                  <p>{card.colors}</p>
+                  <p>{card.magicSetName}</p>
+                  <p>{card.rarity}</p>
+                  <img src={card.image} />
+                  <button onClick={deleteCard} value={card.id}>Delete</button>
+                </Fragment>
+              )
+            : <img src={card.image} />
+        }
       </article>
     );
   }
--- front-end/src/components/CardForm.js
+++ front-end/src/components/CardForm.js
@@ -4,27 +4,51 @@ import PropTypes from 'prop-types';
 class CardForm extends Component {
   render() {
     const { card, onChange } = this.props;
+    const showDetails = window.location.pathname !== '/' ? true : false;
 
     return (
-      <Fragment>
-        <img src={card.image} alt="" />
-        <label htmlFor="name">Name:</label>
-        <input type="text" name="name" value={card.name || ''} onChange={onChange} />
-        <label htmlFor="manaCost">Mana cost:</label>
-        <input type="text" name="manaCost" value={card.manaCost || ''} onChange={onChange} />
-        <label htmlFor="cmc">CMC:</label>
-        <input type="text" name="cmc" value={card.cmc || ''} onChange={onChange} />
-        <label htmlFor="typeLine">Type line:</label>
-        <input type="text" name="typeLine" value={card.typeLine || ''} onChange={onChange} />
-        <label htmlFor="oracleText">Oracle text:</label>
-        <input type="text" name="oracleText" value={card.oracleText || ''} onChange={onChange} />
-        <label htmlFor="colors">Colors:</label>
-        <input type="text" name="colors" value={card.colors || ''} onChange={onChange} />
-        <label htmlFor="magicSetName">Magic set:</label>
-        <input type="text" name="magicSetName" value={card.magicSetName || ''} onChange={onChange} />
-        <label htmlFor="rarity">Rarity:</label>
-        <input type="text" name="rarity" value={card.rarity || ''} onChange={onChange} />
-      </Fragment>
+        showDetails
+          ? (
+            <Fragment>
+            <div>
+              <img src={card.image} alt="" />
+            </div>
+            <div>
+              <label htmlFor="name">Name: </label>
+              <input type="text" name="name" value={card.name || ''} onChange={onChange} />
+            </div>
+            <div>
+              <label htmlFor="manaCost">Mana cost: </label>
+              <input type="text" name="manaCost" value={card.manaCost || ''} onChange={onChange} />
+            </div>
+            <div>
+              <label htmlFor="cmc">CMC: </label>
+              <input type="text" name="cmc" value={card.cmc || ''} onChange={onChange} />
+            </div>
+            <div>
+              <label htmlFor="typeLine">Type line: </label>
+              <input type="text" name="typeLine" value={card.typeLine || ''} onChange={onChange} />
+            </div>
+            <div>
+              <label htmlFor="oracleText">Oracle text: </label>
+              <input type="text" name="oracleText" value={card.oracleText || ''} onChange={onChange} />
+            </div>
+            <div>
+              <label htmlFor="colors">Colors: </label>
+              <input type="text" name="colors" value={card.colors || ''} onChange={onChange} />
+            </div>
+            <div>
+              <label htmlFor="magicSetName">Magic set: </label>
+              <input type="text" name="magicSetName" value={card.magicSetName || ''} onChange={onChange} />
+            </div>
+            <div>
+              <label htmlFor="rarity">Rarity: </label>
+              <input type="text" name="rarity" value={card.rarity || ''} onChange={onChange} />
+            </div>
+          </Fragment>
+          )
+          : <img src={card.image} />
+
     );
   }
 }
