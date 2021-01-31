52a63998f6aa0a7a0659f65968bdbd262c208baf
HCaetano <hrcgeo@gmail.com>
2021-01-31 16:35:11 -0300

    Refactor 'Header' component from class to function

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Logo from '../assets/planeswalker-logo.png';
 import '../css/Header.css';
 
-class Header extends Component {
-  render() {
-    return (
-      <div className="header">
-        <div>
-          <img className="logo" src={Logo} />
-        </div>
+function Header() {
+  return (
+    <div className="header">
+      <div>
+        <img className="logo" src={Logo} />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Header;
