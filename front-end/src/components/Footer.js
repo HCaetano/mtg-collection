caf654c07ee09ca268a7a90a34d8b7cd8d1e4a20
HCaetano <hrcgeo@gmail.com>
2021-01-31 16:34:10 -0300

    Refactor 'Footer' component from class to function

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../css/Footer.css';
 
-class Footer extends Component {
-  render() {
-    return (
-      <footer className="footer">
-        <div>Devolped by Havyner Caetano, {new Date().getFullYear()} &#169;</div>
-      </footer>
-    );
-  }
+function Footer() {
+  return (
+    <footer className="footer">
+      <div>Devolped by Havyner Caetano, {new Date().getFullYear()} &#169;</div>
+    </footer>
+  );
 }
 
 export default Footer;
