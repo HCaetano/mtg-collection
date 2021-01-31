52c029b070383c6e9560a889e7985ac5eb60cb7c
HCaetano <hrcgeo@gmail.com>
2021-01-31 16:41:37 -0300

    Refactor 'CardList' component from class to function

--- front-end/src/components/CardList.js
+++ front-end/src/components/CardList.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Card from '../components/Card';
 import '../css/CardList.css';
 
-class CardList extends Component {
-  render() {
-    const { cards, deleteCard } = this.props;
-
-    return (
-      <section className="card-list">
-        {
-          cards.length > 0
-            ? cards.map((card) => (
-              <Card
-                card={card}
-                key={card.id}
-                deleteCard={deleteCard}
-              />),
-            )
-            : 'There are no cards to show.'
-        }
-      </section>
-    );
-  }
+function CardList({ cards, deleteCard }) {
+  return (
+    <section className="card-list">
+      {
+        cards.length > 0
+          ? cards.map((card) => (
+            <Card
+              card={card}
+              key={card.id}
+              deleteCard={deleteCard}
+            />),
+          )
+          : 'There are no cards to show.'
+      }
+    </section>
+  );
 }
 
 CardList.propTypes = {
