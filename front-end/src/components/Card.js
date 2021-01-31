34671ef85fe1b4372c275b803dd0a0703059291f
HCaetano <hrcgeo@gmail.com>
2021-01-31 16:23:08 -0300

    Refactor 'Card' component from class to function, remove 'Fragment"

--- front-end/src/components/Card.js
+++ front-end/src/components/Card.js
@@ -1,33 +1,30 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import '../css/Card.css';
-class Card extends Component {
-  render() {
-    const { card, deleteCard } = this.props;
-    const showDetails = window.location.pathname !== '/' ? true : false;
+function Card({ card, deleteCard }) {
+  const showDetails = window.location.pathname !== '/' ? true : false;
 
-    return (
-      <article className="card-element">
-        {
-          showDetails
-            ? (
-                <Fragment>
-                  <p>{card.name}</p>
-                  <p>{card.manaCost}</p>
-                  <p>{card.typeLine}</p>
-                  <p>{card.oracleText}</p>
-                  <p>{card.colors}</p>
-                  <p>{card.magicSetName}</p>
-                  <p>{card.rarity}</p>
-                  <img src={card.image} />
-                  <button onClick={deleteCard} value={card.id}>Delete</button>
-                </Fragment>
-              )
-            : <img src={card.image} />
-        }
-      </article>
-    );
-  }
+  return (
+    <article className="card-element">
+      {
+        showDetails
+          ? (
+            <>
+              <p>{card.name}</p>
+              <p>{card.manaCost}</p>
+              <p>{card.typeLine}</p>
+              <p>{card.oracleText}</p>
+              <p>{card.colors}</p>
+              <p>{card.magicSetName}</p>
+              <p>{card.rarity}</p>
+              <img src={card.image} />
+              <button onClick={deleteCard} value={card.id}>Delete</button>
+            </>
+          )
+          : <img src={card.image} />
+      }
+    </article>
+  );
 }
 
 Card.propTypes = {
